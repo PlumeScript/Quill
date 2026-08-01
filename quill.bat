2bad83237bf633fffe28cab9514b4536427a21dc
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-08-01 23:05:05 +0200

    Check git availability at launch

--- quill.bat
+++ quill.bat
@@ -4,5 +4,9 @@
         goto :found
     )
     :found
+    where git >nul 2>nul || (
+        echo Quill requires git. Install git and add it to your PATH.
+        exit /b 1
+    )
     plume -i %~dp0\quill-data\init.plume --future-string --params %* --plumePath=%plumePath%
 exit /b %errorlevel%
